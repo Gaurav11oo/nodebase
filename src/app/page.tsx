61da14d30dffbcb63fdf0b33cd1ee76148a475bf
PROJECT <nishadgaurav2017@gmail.com>
2026-06-05 10:30:11 +0530

    08: Error Tracking

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,6 +17,9 @@ const Page = () => {
       onSuccess: () => {
         toast.success("AI Job Queued");
       },
+      onError: ()=>{
+        toast.error("Something went wrong!!!")
+      }
     })
   );
 
