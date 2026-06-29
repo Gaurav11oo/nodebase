b9b17910d572152c32bfa198d5deb869f7a80e95
PROJECT <nishadgaurav2017@gmail.com>
2026-06-29 16:53:07 +0530

    23 : Stripe Trigger

--- src/components/node-selector.tsx
+++ src/components/node-selector.tsx
@@ -38,6 +38,12 @@ const triggerNodes: NodeTypeOption[] = [
     description: "Runs the flow when a Google Form is Submitted.",
     icon: "/logos/googleform.svg",
   },
+  {
+    type: NodeType.STRIPE_TRIGGER,
+    label: "Stripe Event",
+    description: "Runs the flow when a Stripe Event is Captured.",
+    icon: "/logos/stripe.svg",
+  },
 ];
 
 const executionNodes: NodeTypeOption[] = [
